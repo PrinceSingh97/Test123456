6404a5c1d5818c7a24552133fa94010224b7c238
PrinceSingh97 <61486137+PrinceSingh97@users.noreply.github.com>
2023-02-05 14:49:06 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+{
+  "name": "Heroku Hello World",
+  "description": "A simple Hello World web application for Heroku",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "4.17.1"
+  }
+}
